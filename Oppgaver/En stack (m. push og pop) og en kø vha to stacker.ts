889293f2afabaa2ca797044f6ab438df76889f4e
Ilia-AH <113061886+Ilia-AH@users.noreply.github.com>
2024-02-01 19:06:21 +0100

    Preparing initial practice tasks

---
+++ Oppgaver/En stack (m. push og pop) og en kø vha to stacker.ts
@@ -0,0 +1,7 @@
+/*
+    Jeg kommer til å forelese et enkelt programmeringseksempel med Typescript førstkommende tirsdag 30.01, 
+    så det er veldig bra om dere prøver dere litt først, på egen hånd. '
+    Lag en Typescript-implementasjon av en stack (en "stabel") der det siste elementet som legges inn ("pushes") 
+    er det neste/første kom skal tas ut ("poppes"). Utvide det så med å programmere en "kø",
+    slik at det siste elementet inn også er det siste som tas ut (veldig rettferdig). 
+*/
---
+++ Oppgaver/Litt lett Typescript.ts
@@ -0,0 +1,18 @@
+// Oppgave 1
+// Skriv et program som skriver ut en hilsen til skjermen (konsollet, eller terminalen, om du vil.)
+
+
+// Oppgave 2
+/* Gjør en endring slik at programmet skriver dette til et browservindu 
+(forutsetter vel litt enkel Javascript/html-kunnskap).*/
+
+// Oppgave 3
+// Gjør en endring slik at programmet skriver dette til en «alert»-dialogboks. Bruk ekspress (jfr læreboka).
+
+// Oppgave 4
+// Skriv et program som ber brukeren om navnet sitt og hilser dem med navnet sitt.
+
+// Oppgave 5
+/* Endre det forrige programmet slik at bare brukerne som har en lovlig epostadresse med @himolde 
+blir møtt med navnene sine, de andre kan du f.eks. be om å registrere seg på studiekontoret e.l. 
+(pensumboka inneholder det du trenger i «MailBag»-caset).*/
